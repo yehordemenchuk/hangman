68e7767e5ba91d94957200055df09717b4cadf1b
Yehor Demenchuk <demenchuk1210m@gmail.com>
2024-06-19 21:16:44 +0200

    feat(words-module): add functions to generate secret and output words

---
+++ words.h
@@ -0,0 +1,28 @@
+#ifndef WORDS_H
+#define WORDS_H
+
+#include <string>
+
+using namespace std;
+
+namespace words {
+    const string dictionary_file_name = "dictionary.txt";
+
+    const int words_count = 200;
+
+    const int min_word_number = 1;
+
+    const short inserted_chars_count = 1;
+
+    const char default_insert_char = '_';
+}
+
+void upload_words(string dictionary_words[]);
+
+void random_generator_init();
+
+string generate_secret_word(const string dictionary_words[]);
+
+string generate_output_word(const string secret_word, const string user_input_word);
+
+#endif
